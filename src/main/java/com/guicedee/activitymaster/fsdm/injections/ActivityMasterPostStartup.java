ad55cd0391afcbba1a451f32d48db443fb5dd17a
GedMarc <marc.magon@gmail.com>
2021-10-10 13:49:13 +0200

    Logging and things

--- src/main/java/com/guicedee/activitymaster/fsdm/injections/ActivityMasterPostStartup.java
+++ src/main/java/com/guicedee/activitymaster/fsdm/injections/ActivityMasterPostStartup.java
@@ -2,23 +2,12 @@ package com.guicedee.activitymaster.fsdm.injections;
 
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.google.common.base.Strings;
-import com.google.inject.Inject;
-import com.google.inject.Provider;
-import com.guicedee.guicedinjection.GuiceContext;
-import com.guicedee.guicedinjection.interfaces.IGuicePostStartup;
-import lombok.extern.java.Log;
-import com.guicedee.activitymaster.fsdm.client.services.ConsoleLogActivityMasterProgressMaster;
-import com.guicedee.activitymaster.fsdm.client.services.IEnterpriseService;
-import com.guicedee.activitymaster.fsdm.client.services.administration.ActivityMasterConfiguration;
 import com.guicedee.activitymaster.fsdm.client.services.builders.warehouse.enterprise.IEnterprise;
 import com.guicedee.activitymaster.fsdm.client.services.builders.warehouse.party.*;
 import com.guicedee.activitymaster.fsdm.client.services.deserializers.*;
-import com.guicedee.activitymaster.fsdm.client.services.exceptions.EnterpriseException;
-import com.guicedee.activitymaster.fsdm.client.services.systems.IActivityMasterSystem;
-
-import java.util.Set;
-import java.util.logging.Level;
+import com.guicedee.guicedinjection.GuiceContext;
+import com.guicedee.guicedinjection.interfaces.IGuicePostStartup;
+import lombok.extern.java.Log;
 
 import static com.guicedee.guicedinjection.interfaces.ObjectBinderKeys.*;
 
@@ -36,6 +25,9 @@ public class ActivityMasterPostStartup implements IGuicePostStartup<ActivityMast
 				            .addDeserializer(IInvolvedPartyNameType.class, new InvolvedPartyNameTypeDeserializer())
 				            .addDeserializer(IInvolvedPartyIdentificationType.class, new InvolvedPartyIdentificationTypeDeserializer())
 				            .addDeserializer(IInvolvedParty.class, new InvolvedPartyDeserializer())
+				            
+				            
+				            
 		            );
 	}
 	
