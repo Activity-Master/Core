b69209777e5f4ad050b296b6e8418da5ad5a9134
GedMarc <marc.magon@gmail.com>
2025-03-02 23:41:18 +0200

    Post Startup Updates

--- src/main/java/com/guicedee/activitymaster/fsdm/injections/ActivityMasterPostStartup.java
+++ src/main/java/com/guicedee/activitymaster/fsdm/injections/ActivityMasterPostStartup.java
@@ -2,10 +2,13 @@ package com.guicedee.activitymaster.fsdm.injections;
 
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.google.inject.Inject;
 import com.guicedee.activitymaster.fsdm.client.services.builders.warehouse.enterprise.IEnterprise;
 import com.guicedee.activitymaster.fsdm.client.services.builders.warehouse.party.*;
 import com.guicedee.activitymaster.fsdm.client.services.deserializers.*;
 import com.guicedee.guicedinjection.interfaces.IGuicePostStartup;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 import lombok.extern.java.Log;
 
 import java.util.List;
@@ -16,10 +19,13 @@ import static com.guicedee.guicedinjection.interfaces.ObjectBinderKeys.*;
 @Log
 public class ActivityMasterPostStartup implements IGuicePostStartup<ActivityMasterPostStartup>
 {
+	@Inject
+	private Vertx vertx;
+
 	@Override
-	public List<CompletableFuture<Boolean>> postLoad()
+	public List<Future<Boolean>> postLoad()
 	{
-		return List.of(CompletableFuture.supplyAsync(() -> {
+		return List.of(vertx.executeBlocking(() -> {
 			log.info("Configuration Jackson JSON for types in FSDM");
 			com.guicedee.client.IGuiceContext.get(DefaultObjectMapper)
 			                                 .registerModule(new SimpleModule("ActivityMasterJsonModule", Version.unknownVersion())
@@ -30,7 +36,8 @@ public class ActivityMasterPostStartup implements IGuicePostStartup<ActivityMast
 					                                 .addDeserializer(IInvolvedParty.class, new InvolvedPartyDeserializer())
 			                                 );
 			return true;
-		}, getExecutorService()));
+		}));
+
 	}
 	
 	@Override
